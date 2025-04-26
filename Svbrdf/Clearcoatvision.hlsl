9e65305126e982c268d3e7357545b6d5c2a0dafb
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-26 15:53:16 +0200

    Create Clearcoatvision.hlsl

---
+++ Svbrdf/Clearcoatvision.hlsl
@@ -0,0 +1,4 @@
+//direclty access pbr errors. we can do it by 
+//square hessian matrixes. plus, use not square hessian matrixes 
+//it can simolify first order errors of svbrdf if we dont check
+//heat transfer non related ciphers.
